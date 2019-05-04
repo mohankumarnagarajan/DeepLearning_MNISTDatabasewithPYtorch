6614bffaa8dae00d89176ff033d02c58672faa39
mohankumarnagarajan <mohankumarn@ufl.edu>
2019-05-04 16:14:20 -0700

    Update and rename MNISTPredictionPytorch.py to MNISTDigitsImageClassifi.py

--- MNISTDigitsImageClassifi.py
+++ MNISTDigitsImageClassifi.py
@@ -1,13 +1,15 @@
-%matplotlib inline
+#matplotlib inline
+import matplotlib
 import helper
 import torch
+from collections import OrderedDict
 from torch import nn
 import torch.nn.functional as F
 from torch import optim
 from torchvision import datasets, transforms
 
 # Define a transform to normalize the data
-class MNIST_Pytorch(nn.module):
+class MNIST_DigitsImageClassifier(nn.Module):
     def __init__(self, imgpath='~/.pytorch/MNIST_data/'):
         super().__init__()
         #input layer
@@ -15,62 +17,68 @@ class MNIST_Pytorch(nn.module):
 
         self.criterion = nn.NLLLoss()
         self.imagepath = imgpath
-        self.optimizer = optim.SGD(model.parameters(), lr=0.003)
-        
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.003)
+
     def preprocessImage(self):
         #Loading and preprocessing image
         self.transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.5, ), (0.5, )),
                                   ])
         # Download and load the training data
-        self.trainset = datasets.MNIST(self.imagepath, download=True, train=True, transform=transform)
+        self.trainset = datasets.MNIST(self.imagepath, download=True, train=True, transform=self.transform)
         #trainloader has the normalized image with batch size 64)
-        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+        self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=64, shuffle=True)
 
 
 
-    def trainnetwork(self,ep):
+    def trainnetwork(self,ep=5):
         epochs = ep
+        self.preprocessImage()
         for e in range(epochs):
             running_loss = 0
-            
+
             for images, labels in self.trainloader:
                 # Flatten MNIST images into a 784 long vector
                 images = images.view(images.shape[0], -1)
-            
+
                 output = self.model(images)
-                
+
                 self.optimizer.zero_grad()
                 loss = self.criterion(output,labels)
-                
+
                 loss.backward()
                 self.optimizer.step()
-                
+
                 running_loss += loss.item()
             else:
-                print(f"Training loss: {running_loss/len(trainloader)}")
-        
-                
+                print(f"Training loss: {running_loss/len(self.trainloader)}")
+
+
 
     def testnetwork(self):
         images, labels = next(iter(self.trainloader))
-        
+
         img = images[0].view(1, 784)
         # Turn off gradients to speed up this part
         with torch.no_grad():
-            logps = model(img)
-        
+            logps = self.model(img)
+
         # Output of the network are log-probabilities, need to take exponential for probabilities
         ps = torch.exp(logps)
         helper.view_classify(img.view(1, 28, 28), ps)
-        
-    def main(imgpath):
-        print("Predicting MNIST images using Pytorch neural networks")
-        network = MNIST_Pytorch(imgpath)
-        network.trainnetwork()
-        network.testnetwork()
+
+#    def main(imgpath, ep):
+#        print("Predicting MNIST images using Pytorch neural networks")
+#        network = MNIST_Pytorch(imgpath)
+#        network.trainnetwork(ep)
+#        network.testnetwork()
 
 
 if __name__ == '__main__':
     imgpath = '~/.pytorch/MNIST_data/'
-    main(imgpath)
+    ep = 5
+#    main(imgpath, ep)
+    print("Predicting MNIST images using Pytorch neural networks")
+    network = MNIST_DigitsImageClassifier(imgpath)
+    network.trainnetwork(ep)
+    network.testnetwork()
